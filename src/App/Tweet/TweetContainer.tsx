d20c4c033d3d2adfafdbb0baf51d062af7f79b64
HAL0805 <micky.hallabrin@stateauto.com>
2019-02-27 15:56:18 -0500

    changed to pure component

--- src/App/Tweet/TweetContainer.tsx
+++ src/App/Tweet/TweetContainer.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import Tweet from './Tweet';
 import TweetModel from './models/Tweet.model';
 
-export default class TweetContainer extends React.Component<{}, {tweet: TweetModel}> {    
+export default class TweetContainer extends React.PureComponent<{}, {tweet: TweetModel}> {    
     state = {
         tweet: new TweetModel()
     };
-    initialModel: TweetModel = new TweetModel();
 
     constructor(props: any) {
         super(props);
@@ -22,17 +21,22 @@ export default class TweetContainer extends React.Component<{}, {tweet: TweetMod
                 retweets: 5,
                 timestamp: "2016-07-30 21:24:37"
             })
-        }
-        Object.assign(this.initialModel, this.state.tweet);
-    }
+        };
 
-    shouldComponentUpdate(prev: any, change: any) {
-        if ((prev && prev.likes !== change.tweet.likes) 
-            || (this.initialModel.likes !== change.tweet.likes)) {
-            return true;
-        } else {
-            return false;
-        }
+        setTimeout(() => {
+            const newState = new TweetModel({
+                message: "something about cats.",
+                gravatar: "xyz",
+                author: {
+                    handle: "catperson",
+                    name: "IAMA Cat Person"
+                },
+                likes: 7,
+                retweets: 5,
+                timestamp: "2016-07-30 21:24:37"
+            })
+            this.setState({tweet: newState});
+        }, 2000);
     }
   
     render() {
